bee92a5cff9f601d54b00fe962a38703f4ad6feb
BB Caspian <casfian@gmail.com>
2021-07-29 13:34:02 +0800

    added  container and accent colors

--- lib/screens/phomestab.dart
+++ lib/screens/phomestab.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:page_indicator/page_indicator.dart';
-import 'package:prima/screens/page1.dart';
-import 'package:prima/screens/page2.dart';
-import 'package:prima/screens/page3.dart';
  
 class PHomesTab extends StatelessWidget {
   final _controller = PageController();
 
   List<Widget> pages = [
-    Page1(),
-    Page2(),
-    Page3()
+    Container(color: Colors.grey,),
+    Container(color: Colors.purpleAccent,),
+    Container(color: Colors.greenAccent,)
   ];
 
   @override
